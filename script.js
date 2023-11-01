05984f1b779b6e07b43bd70598bc92c8c3562926
olegAN97 <oleganx16@gmail.com>
2023-11-01 13:34:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+myFunction(){
+"use strict";
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.forwardemail.net",
+  port: 465,
+  secure: true,
+  auth: {
+    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+    user: 'REPLACE-WITH-YOUR-ALIAS@YOURDOMAIN.COM',
+    pass: 'REPLACE-WITH-YOUR-GENERATED-PASSWORD'
+  }
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "bar@example.com, baz@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+
+}
+
+main().catch(console.error);
+}
